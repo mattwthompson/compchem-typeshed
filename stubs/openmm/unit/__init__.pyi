da03088882c33adf0decbc7fb9508362f5340bf6
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-02-12 11:03:14 -0600

    Use explicit paths in openmm imports

--- stubs/openmm/unit/__init__.pyi
+++ stubs/openmm/unit/__init__.pyi
@@ -1,8 +1,8 @@
-from .unit_math import *
-from .unit_definitions import *
-from .constants import *
-from .quantity import Quantity as Quantity, is_quantity as is_quantity
-from .unit import Unit as Unit, is_unit as is_unit
+from openmm.unit.unit_math import *
+from openmm.unit.unit_definitions import *
+from openmm.unit.constants import *
+from openmm.unit.quantity import Quantity as Quantity, is_quantity as is_quantity
+from openmm.unit.unit import Unit as Unit, is_unit as is_unit
 from typing import Any
 
 __docformat__: str
