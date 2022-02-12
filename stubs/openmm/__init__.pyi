823a16ed60bfb115996e7599b17ccab2dcca62d5
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-02-12 12:02:01 -0600

    Add openmm/openmm.pyi via stubgen and tweak

--- stubs/openmm/__init__.pyi
+++ stubs/openmm/__init__.pyi
@@ -3,9 +3,7 @@ from openmm.unit.unit_definitions import *
 from openmm.unit.constants import *
 from openmm.unit.quantity import Quantity as Quantity, is_quantity as is_quantity
 from openmm.unit.unit import Unit as Unit, is_unit as is_unit
-from openmm._forces import *
-from openmm._system import *
-from openmm._misc import *
+from openmm.openmm import *
 from typing import Any
 
 __docformat__: str
--- stubs/openmm/unit/doctests.pyi
+++
@@ -1,7 +0,0 @@
-from standard_dimensions import *
-from unit_definitions import *
-from unit_math import *
-from constants import *
-from baseunit import BaseUnit as BaseUnit
-from quantity import Quantity as Quantity, is_dimensionless as is_dimensionless, is_quantity as is_quantity
-from unit import dimensionless as dimensionless, is_unit as is_unit
